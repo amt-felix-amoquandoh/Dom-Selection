ccc34ed8fd22e1a9e7d62e5723a316d1a12fb5ab
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-07-28 04:17:46 +0000

    initial commit

---
+++ select Item/js/sript.js
@@ -0,0 +1,15 @@
+document.querySelector("#listGroup").addEventListener("click", function(e){
+    const target = e.target;
+    if(target.matches("li")){
+        target.style.backgroundColor = "orange"
+    }
+})
+
+
+const listGroup = document.getElementById("listGroup");
+const newMovie = document.createElement("li");
+
+newMovie.innerText = prompt("add movie");
+newMovie.setAttribute("class", "list-group-item");
+
+listGroup.appendChild(newMovie);
