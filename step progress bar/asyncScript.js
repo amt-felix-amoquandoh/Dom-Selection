86b66c5e03ca34fa0197a05261c72a0235192127
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-08-25 10:59:52 +0000

    first commit

---
+++ step progress bar/asyncScript.js
@@ -0,0 +1,4 @@
+const firstForm = document.getElementById("form1");
+const secondFrom = document.getElementById("form2");
+const thirdForm = document.getElementById("form3");
+const fourthForm = document.getElementById("form4");
