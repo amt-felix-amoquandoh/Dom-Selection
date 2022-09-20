87130918c6760a3c4d7899dd31d78e8266c0360a
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-09-20 11:28:04 +0000

    project practice update

--- js/sript.js
+++ js/sript.js
@@ -1,9 +1,16 @@
-document.querySelector("#listGroup").addEventListener("click", function(e){
+let group1 = [];
+let group2 = [];
+let moveLeft = document.getElementsByClassName("btnLeft");
+let moveRight = document.getElementsByClassName("btnright");
+
+document.addEventListener("click", function(e){
     const target = e.target;
     if(target.matches("li")){
         target.style.backgroundColor = "orange"
     }
 })
+
+
 
 
 // const listGroup = document.getElementById("listGroup");
