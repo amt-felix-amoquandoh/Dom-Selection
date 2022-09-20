c5f58c621550a7ced070456c1546c78b7718452d
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-09-20 14:07:13 +0000

    Update sript.js

--- js/sript.js
+++ js/sript.js
@@ -2,6 +2,7 @@ let group1 = [];
 let group2 = [];
 let moveLeft = document.getElementsByClassName("btnLeft");
 let moveRight = document.getElementsByClassName("btnRight");
+let addMovie = document.getElementById("addBtn");
 
 function allEventListeners(type, selector, callback){
     document.addEventListener(type, e => {
@@ -9,9 +10,20 @@ function allEventListeners(type, selector, callback){
     })
 }
 
-allEventListeners("click", "li", e => {e.target.classList.toggle("listSelect")})
+allEventListeners("click", "li", e => {e.target.classList.toggle("listSelect")});
 allEventListeners("click", "ion-icon", e => {console.log("guyyyyyyyyyyy")})
 
+addMovie.addEventListener("click", function(){
+    prompt("Enter Movie")
+})
+
+moveRight.addEventListener("click", function(){
+    console.log("right")
+})
+
+moveLeft.addEventListener("click", function(){
+    console.log("left")
+})
 
 
 // const listGroup = document.getElementById("listGroup");
