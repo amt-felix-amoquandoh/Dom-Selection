831c8b5c03dea50939a801a84159ba4876a4f5c9
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-09-27 21:26:51 +0000

    project files update

--- js/sript.js
+++ js/sript.js
@@ -1,21 +1,35 @@
 let newMovie = document.getElementById("newMovie");
+let removeMovie = document.getElementById("removeBtn");
 let movieList = document.getElementById("newMovies");
 let watchedMovies = document.getElementById("watchedMovies");;
 let moveLeft = document.getElementsByClassName("btnLeft");
 let moveRight = document.getElementsByClassName("btnRight");
 let addMovie = document.getElementById("addBtn");
+let listedMovie = document.querySelector("li");
 
 //general event listener
-function allEventListeners(type, selector, callback){
-    document.addEventListener(type, e => {
-       if (e.target.matches(selector)) callback(e)
-    })
-}
+let selectedMovie = listedMovie.addEventListener("click", function(e){
+    const target = e.target;
+    if (target.matches("li")){
+        target.classList.toggle("listSelect")
+    }
+    return selectedMovie;
+})
 
-allEventListeners("click", "li", e => {e.target.classList.toggle("listSelect")});
-allEventListeners("click", "ion-icon", e => {console.log("guyyyyyyyyyyy")})
+
+// function allEventListeners(type, selector, callback){
+//     document.addEventListener(type, e => {
+//        if (e.target.matches(selector)) callback(e)
+//     })
+// }
 
 
+
+// allEventListeners("click", "li", e => {e.target.classList.toggle("listSelect")});
+// allEventListeners("click", "ion-icon", e => {console.log("guyyyyyyyyyyy")})
+
+addMovie.addEventListener("click", addMovieToList);
+removeMovie.addEventListener("click", removeFromList);
 
 //input function
 function movieInput (){   
@@ -23,7 +37,7 @@ function movieInput (){
 }
 
 
-
+// adding new movie to DOM
 function addNewMovie (){
     var movie = document.createElement("li");
     movie.appendChild(document.createTextNode(newMovie.value));
@@ -38,15 +52,13 @@ function addMovieToList (){
 }
 }
 
-function addOnEnterKeypress(event) {
-        if(movieInput() > 1 && event.key === "Enter" ){
-            addNewMovie();
-            event.preventDefault();
-    }
+//removing movie from list
+function removeFromList(){
+    selectedMovie.remove();
 }
 
-addMovie.addEventListener("click", addMovieToList);
-addMovie.addEventListener("keypress", addOnEnterKeypress);
+
+
 
 
 
