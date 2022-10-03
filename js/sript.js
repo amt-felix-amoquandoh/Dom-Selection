3a998f5e8e68bc1a150c2a5550b5f7a71626f0df
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-10-03 15:51:35 +0000

    dom events update

--- js/sript.js
+++ js/sript.js
@@ -1,64 +1,62 @@
-const newMovie = document.getElementById("newMovie");
-const removeMovie = document.getElementById("removeBtn");
-const movieList = document.getElementById("newMovies");
-const watchedMovies = document.getElementById("watchedMovies");;
-const moveLeft = document.getElementsByClassName("btnLeft");
-const moveRight = document.getElementsByClassName("btnRight");
-const addMovie = document.getElementById("addBtn");
-const listedMovie = document.querySelector("li");
-
-let myMovies = []; 
-
-//general event listener
-let selectedMovie = listedMovie.addEventListener("click", function(e){
-    const target = e.target;
-    if (target.matches("li")){
-        target.classList.toggle("listSelect")
-    }
-    return selectedMovie;
-})
-
-
 // function allEventListeners(type, selector, callback){
 //     document.addEventListener(type, e => {
 //        if (e.target.matches(selector)) callback(e)
 //     })
 // }
-
-
-
 // allEventListeners("click", "li", e => {e.target.classList.toggle("listSelect")});
 // allEventListeners("click", "ion-icon", e => {console.log("guyyyyyyyyyyy")})
 
-addMovie.addEventListener("click", addMovieToList);
-removeMovie.addEventListener("click", removeFromList);
 
-//input function
-function movieInput (){   
-    return newMovie.value.length
+
+let myMovies = [];
+let AlreadySeenMovies = []; 
+let newMovieInput = document.getElementById("newMovieInput");
+let addNewMovie = document.getElementById("addNewBtn");
+let newMoviesList = document.getElementById("newMoviesList");
+
+// let removeMovie = document.getElementById("removeBtn");
+// let watchedMovies = document.getElementById("watchedMovies");;
+// let moveLeft = document.getElementsByClassName("btnLeft");
+// let moveRight = document.getElementsByClassName("btnRight");
+
+// let listedMovie = document.querySelector("li");
+
+// //general event listener
+// let selectedMovie = listedMovie.addEventListener("click", function(e){
+//     const target = e.target;
+//     if (target.matches("li")){
+//         target.classList.toggle("listSelect")
+//     }
+//     return selectedMovie;
+// })
+
+let storedMovies = JSON.parse(localStorage.getItem("myMovies")); 
+if(storedMovies){
+    myMovies = storedMovies;
+    displayMovies();
+}
+
+function addMovieToList (){    
+    if(newMovieInput.value.length > 1){
+        myMovies.push(newMovieInput.value);
+    }  
+    newMovieInput.value = "";
+    localStorage.setItem("myMovies", JSON.stringify(myMovies));
+    displayMovies();  
+}
+
+function displayMovies (){
+    let movies = "";
+    for(let i = 0;i < myMovies.length; i++){
+        movies += "<li class='list-group-item'>" + myMovies[i] + "</li>"
+    }
+    newMoviesList.innerHTML = movies;
 }
 
 
-// adding new movie to DOM
-function addNewMovie (){
-    var movie = document.createElement("li");
-    movie.appendChild(document.createTextNode(newMovie.value));
-    movieList.appendChild(movie);
-    movie.classList.add("list-group-item")
-    newMovie.value = "";  
-}
 
-function addMovieToList (){
-    if(movieInput() > 0){
-        addNewMovie();
-}
-}
 
-//removing movie from list
-function removeFromList(){
-    selectedMovie.remove();
-}
-
+addNewMovie.addEventListener("click", addMovieToList);
 
 
 
