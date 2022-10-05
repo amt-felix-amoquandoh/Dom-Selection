66e13dfc8ebfbb1fe4def7374f55630fdd4be6e2
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-10-05 12:58:34 +0000

    index and script update

--- js/sript.js
+++ js/sript.js
@@ -5,6 +5,7 @@ let addNewMovie = document.getElementById("addNewBtn");
 let newMoviesList = document.getElementById("newMoviesList");
 let selectedMovie = document.querySelector(".list-group");
 let removeMovies = document.getElementById("removeBtn");
+let closeMovie = document.getElementsByClassName("closeBtn");
 
 
 const storedMovies = JSON.parse(localStorage.getItem("myMovies")); 
@@ -12,15 +13,6 @@ if(storedMovies){
     myMovies = storedMovies;
     displayMovies();
 }
-
-// function addMovieToList (){    
-//     if(newMovieInput.value.length > 1){
-//         myMovies.push(newMovieInput.value);
-//     }  
-//     newMovieInput.value = "";
-//     localStorage.setItem("myMovies", JSON.stringify(myMovies));
-//     displayMovies();  
-// }
 
 
 function addMovieToList (){    
@@ -33,28 +25,25 @@ function addMovieToList (){
 }
 
 
-
-
 function displayMovies (){
     let movies = "";
     for(let i = 0;i < myMovies.length; i++){
         movies += "<li class='list-group-item'>" + 
         myMovies[i] +
-        "<ion-icon class='closeBtn' type='button' name='close-circle-outline' onclick='moveMovie()'></ion-icon>" + 
+        "<ion-icon class='closeBtn' type='button' name='close-circle-outline'></ion-icon>" + 
         "</li>"
     }
-    newMoviesList.innerHTML = movies;
-    console.log(movies.parentNode)
+    newMoviesList.innerHTML = movies;    
 }
 
-function moveMovie(film){
-    
+function moveMovie(){
+    console.log("trieeeeeeeee")
 }
 
 
 function selectMovie (e){
     let target = e.target;
-    if (target.matches("li")){
+    if (target.contains("li")){
          target.classList.toggle("listSelect");
     }
 }
@@ -65,7 +54,7 @@ function removeAllMovies (){
     displayMovies();
 }
 
-
+closeMovie.addEventListener("click", moveMovie)
 removeMovies.addEventListener("click", removeAllMovies)
 selectedMovie.addEventListener("click", selectMovie);
 addNewMovie.addEventListener("click", addMovieToList);
