35db258d7876fdabadf29a3732f0faaa95807521
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-10-26 10:54:31 +0000

    project files update

--- js/sript.js
+++ js/sript.js
@@ -1,15 +1,7 @@
 document.querySelector("#listGroup").addEventListener("click", function(e){
     const target = e.target;
     if(target.matches("li")){
-        target.style.backgroundColor = "orange"
-    }
+        target.classList.toggle("selectedItem");
+    }  
 })
 
-
-// const listGroup = document.getElementById("listGroup");
-// const newMovie = document.createElement("li");
-
-// newMovie.innerText = prompt("add movie");
-// newMovie.setAttribute("class", "list-group-item");
-
-// listGroup.appendChild(newMovie);
